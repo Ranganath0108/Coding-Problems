c80ac7b2a6c4d03cdfc489c24f8876c363c303f1
Ranganath0108 <ranganathswamy.ys@gmail.com>
2022-04-18 22:38:50 +0530

    New Problems and tree Files added

--- test.py
+++ test.py
@@ -1,23 +1,20 @@
-def minJump(arr,n):
- 
-    # unoptimized and non usable code for all testcases
-    i=0
-    count=0
-    while(i<n):
-        jump=arr[i]
-        i+=jump
-        if(i>=n-1):
-            count+=1
-            return count
-        else:
-            count+=1
-    return -1
-
-
-
-def minJump1(arr,n):
-    pass
-
-           
-
-print(minJump1([1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9],11))
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+# Replace below path with the absolute path
+browser = webdriver.Chrome('chromedriver.exe')
+phone = "9591091911"
+message = "Hii am Ranganath From KIT \n How was your coding round what are the question ,They asked??"
+url = "https//:wa.me/91{phone}"
+# Load Whatsapp Web page
+browser.get(url)
+# Wait for the page be loaded
+time.sleep(10)
+enter_action = ActionChains(browser)
+enter_action.send_keys(Keys.ENTER)
+# Send message
+enter_action.perform()
+# Close browser
+browser.close()
